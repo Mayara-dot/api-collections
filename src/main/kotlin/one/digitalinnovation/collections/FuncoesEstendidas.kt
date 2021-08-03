257f660598d71c4cd76807e3039336c2a79c0bb2
Mayara-dot <mayara_slima@hotmail.com>
2021-08-03 12:51:40 -0300

    Created Extentions Functions

---
+++ src/main/kotlin/one/digitalinnovation/collections/FuncoesEstendidas.kt
@@ -0,0 +1,11 @@
+package one.digitalinnovation.collections
+
+import java.math.BigDecimal
+
+fun Array<BigDecimal>.somatoria() = this.reduce {
+    add, valor -> add + valor
+} //Extension function
+
+fun Array<BigDecimal>.media() =
+    if(this.isEmpty()) BigDecimal.ZERO
+    else this.somatoria() / this.size.toBigDecimal()  //Extension function
